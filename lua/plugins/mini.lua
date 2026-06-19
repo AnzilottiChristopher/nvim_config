bb84ec1e37d0d074be49a1a6ce45da8011d910a0
Chris Anzilotti <anzilottichr@gmail.com>
2026-06-18 21:40:36 -0400

    Delete full line if line has nothing on it

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,14 +2,30 @@ return {
     "echasnovski/mini.nvim",
     version = false,
     config = function()
-        -- Text objects and editing
-        require("mini.ai").setup()        -- Better text objects
-        require("mini.surround").setup()  -- Surround text
-        require("mini.move").setup()      -- Move lines with Alt+hjkl
-        require("mini.comment").setup()   -- Commenting
-        require("mini.bufremove").setup() -- Better buffer deletion (works with bufferline!)
+        require("mini.ai").setup()
+        require("mini.surround").setup()
+        require("mini.move").setup()
+        require("mini.comment").setup()
+        require("mini.bufremove").setup()
         require("mini.pairs").setup()
-        -- Update your buffer delete keybind to use mini.bufremove
+
+        vim.keymap.set('i', '<BS>', function()
+            local line = vim.api.nvim_get_current_line()
+            if line:match('^%s*$') then
+                vim.schedule(function()
+                    local row = vim.api.nvim_win_get_cursor(0)[1]
+                    vim.api.nvim_del_current_line()
+                    if row > 1 then
+                        local prev_line = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1]
+                        vim.api.nvim_win_set_cursor(0, { row - 1, #prev_line })
+                    end
+                end)
+                return ''
+            else
+                return MiniPairs.bs()
+            end
+        end, { expr = true, replace_keycodes = false })
+
         vim.keymap.set("n", "<leader>bd", function()
             require("mini.bufremove").delete()
         end, { desc = "Delete Buffer" })
