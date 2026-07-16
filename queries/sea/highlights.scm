69860380fd1e0e78bcb687d551c4c7ca08319550
Chris Anzilotti <anzilottichr@gmail.com>
2026-07-16 16:18:47 -0400

    Updating highlights.scm

--- queries/sea/highlights.scm
+++ queries/sea/highlights.scm
@@ -1,48 +1,42 @@
 ; this — like self in Rust
 (this_expression) @variable.builtin
-
 ; method calls
 (call_expression
   function: (field_expression
     field: (field_identifier) @function.method.call))
-
-; Types
-(base_type) @type
-
+; Types — primitive and class types
+(primitive_type
+  base: _ @type)
+(class_type
+  base: (identifier) @type)
+; Ownership sigils
+(class_type
+  ownership: _ @operator)
 ; Class and interface names
 (class_declaration
   name: (identifier) @type)
-
 (interface_declaration
   name: (identifier) @type)
-
 ; Functions and methods
 (function_definition
   declarator: (function_declarator
     declarator: (identifier) @function))
-
 ; Import path
 (import_declaration
   path: (string) @string.special)
-
 ; Fields
 (field_declaration
   name: (identifier) @variable.member)
-
 ; Parameters
 (sea_parameter
   name: (identifier) @variable.parameter)
-
 ; Identifiers
 (identifier) @variable
-
 ; Literals
 (string_literal) @string
 (number_literal) @number
-
 ; Comments
 (comment) @comment
-
 ; Operators
 "->" @operator
 "*" @operator
@@ -58,7 +52,6 @@
 ">" @operator
 "<=" @operator
 ">=" @operator
-
 ; Keywords — after identifiers so they take priority
 "class" @keyword
 "interface" @keyword
@@ -74,18 +67,16 @@
 "else" @keyword
 "while" @keyword
 "for" @keyword
+"in" @keyword
 "switch" @keyword
 "case" @keyword
 "break" @keyword
 "continue" @keyword
 "void" @keyword
-
 ; Methods — after identifiers so @function.method overrides @variable
 (sea_style_method
   name: (identifier) @function.method)
-
 (c_style_method
   name: (identifier) @function.method)
-
 ; init constructor — after identifiers
 "init" @function.method
